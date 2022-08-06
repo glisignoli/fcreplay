dad9268940f7f4e1eaf5212a4fa7dce0de2e2d39
Gino Lisignoli <glisignoli@oss-group.co.nz>
2022-08-06 18:14:38 +1200

    Testing opentelemtry

--- fcreplay/site/app.py
+++ fcreplay/site/app.py
@@ -2,9 +2,17 @@ from fcreplay.site.create_app import create_app
 from fcreplay.site.site_config import DevConfig, ProdConfig
 from flask.helpers import get_debug_flag, get_env
 
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry import trace
+
+import fcreplay.site.tracing
+
 if get_debug_flag() or get_env != "production":
     CONFIG = DevConfig
 else:
     CONFIG = ProdConfig
 
 app = create_app(CONFIG)
+
+print("Starting open telemetry instrumentation")
+FlaskInstrumentor().instrument_app(app)
